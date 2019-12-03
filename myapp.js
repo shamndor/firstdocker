a96ccbff13681cc21233fecc03407bc43c767545
Ahmed M. Abdelalim <aabdelalim@thiqah.sa>
2019-12-03 13:57:43 +0300

    init commit

---
+++ myapp.js
@@ -0,0 +1,9 @@
+var http = require('https');
+var rand = Math.floor(Math.random() * 100);
+//create a server object:
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('Hello World!'); //write a response to the client
+res.write('My number is: ' + rand); //write a response to the client
+res.end(); //end the response
+}).listen(80); //the server object listens on port 80
