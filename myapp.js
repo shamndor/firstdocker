8f20b4d581e197bab0b900c8e5810a53a571167d
Ahmed M. Abdelalim <aabdelalim@thiqah.sa>
2019-12-10 16:15:41 +0300

    Roll Out

--- myapp.js
+++ myapp.js
@@ -1,9 +1,13 @@
 var http = require('http');
-var rand = Math.floor(Math.random() * 100);
-//create a server object:
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('Hello World!'); //write a response to the client
-res.write('My number is: ' + rand); //write a response to the client
-res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(80);
+ 
+ 
+ 
